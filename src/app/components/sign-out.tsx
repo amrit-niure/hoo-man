9469ffc60c3bd7548e347e4ae4886eb80c82be04
Amrit Niure <niureamrit2@gmail.com>
2025-03-14 00:42:01 +1100

    not working

---
+++ src/app/components/sign-out.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function SignOut() {
+  const router = useRouter();
+  return (
+    <button
+      className="px-4 py-2 rounded-sm bg-slate-100 text-sm  cursor-pointer hover:bg-slate-200"
+      onClick={async () => {
+        await authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/login"); // redirect to login page
+            },
+          },
+        });
+      }}
+    >
+      Sign Out
+    </button>
+  );
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  secret: process.env.BETTER_AUTH_SECRET,
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false
+  },
+  plugins: [nextCookies()] 
+});
